336773cd04f4948a24faf3cdf73499834e7cec46
marzuk <marzuk.islam@dsinnovators.com>
2026-04-09 15:36:58 +0600

    feat: add shared constants package to lib/components (#30)
    Add KafkaTopics, AppHeaders, and Roles constant classes under
    com.marzuk.components.constants so all services reference a single
    source of truth for topic names, header keys, and role strings.

---
+++ lib/components/src/main/java/com/marzuk/components/constants/Roles.java
@@ -0,0 +1,11 @@
+package com.marzuk.components.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.NONE)
+public class Roles {
+
+    public static final String USER = "ROLE_USER";
+    public static final String ADMIN = "ROLE_ADMIN";
+}
