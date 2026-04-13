e0de9366f3e72dbf4a9f8439e9646b0cc63bfeb0
marzuk <marzuk.islam@dsinnovators.com>
2026-04-13 16:14:28 +0600

    feat: bootstrap storage-service with datasource, security, and MinIO wiring (#112)
    Adds StorageApp, MinioConfig, application.yml, banner, and integration tests
    verifying context load and actuator health against a Testcontainers PostgreSQL instance.

---
+++ packages/storage/src/main/java/com/marzuk/storage/StorageApp.java
@@ -0,0 +1,21 @@
+package com.marzuk.storage;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+
+@Slf4j
+@SpringBootApplication(
+        scanBasePackages = {"com.marzuk.storage", "com.marzuk.components"},
+        exclude = {UserDetailsServiceAutoConfiguration.class}
+)
+public class StorageApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageApp.class, args);
+        log.info("###################################");
+        log.info("Storage service started successfully!!!");
+        log.info("###################################");
+    }
+}
