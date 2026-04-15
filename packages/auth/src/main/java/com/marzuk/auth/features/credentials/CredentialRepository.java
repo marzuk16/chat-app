b48419807b6c2c242933c6492ab1712aeba9b618
marzuk <marzuk.islam@dsinnovators.com>
2026-04-15 13:02:23 +0600

    style: add @Repository annotation to CredentialRepository (#40)

--- packages/auth/src/main/java/com/marzuk/auth/features/credentials/CredentialRepository.java
+++ packages/auth/src/main/java/com/marzuk/auth/features/credentials/CredentialRepository.java
@@ -4,7 +4,9 @@ import com.marzuk.auth.entities.Credential;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CredentialRepository extends JpaRepository<Credential, UUID> {
 
     Optional<Credential> findByEmail(String email);
